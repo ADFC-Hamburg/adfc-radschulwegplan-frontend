aebad56ec3f542a9c8c1e4a6bc225c6acdf0a385
James Twellmeyer <james.edward@twellmeyer.eu>
2018-02-11 20:08:51 +0100

    Added main map to app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { YagaModule } from '@yaga/leaflet-ng2';
 
 
 import { AppComponent } from './app.component';
+import { MainMapComponent } from './main-map/main-map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YagaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
