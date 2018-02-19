31044a214ce58f3e33b160e99e2e5b7a06390b16
James Twellmeyer <james.edward@twellmeyer.eu>
2018-02-19 23:35:47 +0100

    Added content to footer and sidebar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { YagaModule } from '@yaga/leaflet-ng2';
 
 import { AppComponent } from './app.component';
 import { MainMapComponent } from './main-map/main-map.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMapComponent
+    MainMapComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
