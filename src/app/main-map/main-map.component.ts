7c40e55c152fe45b7062ffca62c3199caeb4ca14
Sven Anders <github-commit@sven.anders.im>
2018-03-03 18:06:41 +0100

    Anzeigen von 2 Beispiel Danger-Points auf der Karte

--- src/app/main-map/main-map.component.ts
+++ src/app/main-map/main-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DangerPoint } from '../shared/danger-point';
 @Component({
   selector: 'app-main-map',
   templateUrl: './main-map.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMapComponent implements OnInit {
 
+  danger_points: DangerPoint[];
   constructor() { }
 
   ngOnInit() {
+    this.danger_points= [
+      new DangerPoint(
+        53.5,10,'Bordstein zu hoch', 'hier sollte der Bordstein abgesenkt werden, weil sonst die Rollstuhlfahrer aus dem Behindertenheim über den Radweg fahren',42),
+      new DangerPoint(
+        53.51,9.9,'Toter Winkel durch Werbeschild', 'Die Reklame verhindert, das Radfahrer aus der Ausfahrt gesehen werden.',12)
+        ];
   }
 
 }
---
+++ src/app/shared/danger-point.ts
@@ -0,0 +1,9 @@
+export class DangerPoint {
+       constructor(
+       public lat: number,
+       public lon: number,
+       public title: string,
+       public description: string,
+       public typeId: number
+       ) {}
+}
