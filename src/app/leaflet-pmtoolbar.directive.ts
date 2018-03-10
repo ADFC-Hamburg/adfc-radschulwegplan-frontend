cafeefdf52d40bb327331d3a665bc46f6722867d
Sven Anders <github-commit@sven.anders.im>
2018-03-10 12:47:42 +0100

    add marker icon

--- src/app/leaflet-pmtoolbar.directive.ts
+++ src/app/leaflet-pmtoolbar.directive.ts
@@ -27,7 +27,26 @@ export class LeafletPmtoolbarDirective  {
             editMode: true, // adds button to toggle edit mode for all layers
             removalMode: true, // adds a button to remove layers
         };
-        (mapProvider.ref as any).pm.addControls(options);
+        var pm=(mapProvider.ref as any).pm;
+        pm.addControls(options);
+        var greenIcon = L.icon({
+            iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+            shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+
+            iconSize:     [38, 95], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+        pm.enableDraw('Marker', {
+            snappable: false
+            opacity: 0.5,
+            draggable: true,
+            markerStyle: {
+                icon: greenIcon
+            }
+        });
 /*        mapProvider.ref = this;
         this.addTo(parentMapProvider.ref);
         debugger;*/
