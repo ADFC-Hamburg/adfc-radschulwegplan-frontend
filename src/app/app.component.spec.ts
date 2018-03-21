36e00e7fb432cf5a24c5c197156a89f47e32b18e
Sven Anders <sven@anders-hamburg.de>
2018-03-21 13:20:37 +0100

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Radschulwegplan'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Radschulwege');
+    expect(app.title).toEqual('Radschulwegplan');
   }));
 /*  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
